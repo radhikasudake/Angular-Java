1a28be5f5fbd89d2e2b595b685f27353c5fd7313
Radhika Sudake <sudakeradhika50@gmail.com>
2019-11-24 08:50:52 +0530

    my first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>HI radhika</h1>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'radhika';
+}
